e831580c5f1e7634cf5f97251cdbbfa4c2093afc
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-12-09 23:37:54 +0530

    Fixed permission

--- cd-scripts/appStart.sh
+++ cd-scripts/appStart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 755 /home/ec2-user/contacts-app
+sudo chmod -R 777 /home/ec2-user/contacts-app
 
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/contacts-app
