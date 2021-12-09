e7141fd4fb3f594bfc9b995d98bc9a15b7e654a4
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-12-09 23:55:47 +0530

    added echo

--- cd-scripts/appStart.sh
+++ cd-scripts/appStart.sh
@@ -8,9 +8,14 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+
+echo "Installing pm2"
 npm install pm2 -g
 
+echo "Installing other packages"
 npm install
 
+
+echo "----Starting the Server---"
 # sudo npm start &
 sudo pm2 start npm -- start
