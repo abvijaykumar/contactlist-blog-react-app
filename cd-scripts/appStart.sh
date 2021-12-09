6d4509fa07ab65273b64be4a8f40a9ad06e81fdc
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-12-09 22:48:39 +0530

    fixed the port

--- cd-scripts/appStart.sh
+++ cd-scripts/appStart.sh
@@ -11,10 +11,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-export PORT=80
+
 
 #install node modules
 npm install
 
 #start our node app in the background
-sudo npm start
+export PORT=80
+sudo npm start &
