6c9b635f547db7d8ef64bbb12fd1a19696d3fb0b
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-12-09 23:52:17 +0530

    fixed pm2

--- cd-scripts/appStart.sh
+++ cd-scripts/appStart.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/contacts-app
 
-#navigate into our working directory where we have all our github files
+sudo chmod -R 777 /home/ec2-user/contacts-app
 cd /home/ec2-user/contacts-app
 
-#add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-
-#install node modules
-npm install
 npm install pm2 -g
 
-#start our node app in the background
+npm install
+
 # sudo npm start &
 sudo pm2 start npm -- start
