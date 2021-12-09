f1750344e4ed4869dc1a0aebbd313c4116d930fb
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-12-09 23:35:59 +0530

    fixed the permissions

--- cd-scripts/appStart.sh
+++ cd-scripts/appStart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/contacts-app
+sudo chmod -R 755 /home/ec2-user/contacts-app
 
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/contacts-app
@@ -10,7 +10,6 @@ cd /home/ec2-user/contacts-app
 export NVM_DIR="$HOME/.nvm"	
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
 
 
 #install node modules
