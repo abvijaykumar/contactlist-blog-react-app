05e92b7524a30595be2ac267ce0e65461a726857
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-12-10 16:30:28 +0530

    Fixed the AWS Config

--- src/App.js
+++ src/App.js
@@ -2,18 +2,7 @@
 import React, { useEffect, useState } from 'react';
 
 const AWS = require('aws-sdk');
-
-AWS.config.getCredentials(function(err) {
-  if (err) console.log(err.stack);
-  // credentials not loaded
-  else {
-    console.log("Access key:", AWS.config.credentials.accessKeyId);
-    AWS.config.update({region: "us-east-1"});
-  }
-});
-
-
-
+AWS.config.region = 'us-east-1';
 
 var table_name = "contacts-table" 
 
