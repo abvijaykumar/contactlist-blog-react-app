4c1fd333c1666399e7f0f043bf217c73d5befd52
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-12-20 18:27:31 +0530

    Added Styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 
 import React, { useEffect, useState } from 'react';
 
+import {createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+
+const theme = createTheme({
+  spacing: (factor) => `${0.25 * factor}rem`, // (Bootstrap strategy)
+});
+
+theme.spacing(2); // = 0.25 * 2rem = 0.5rem = 8px
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
 function App() {
 
   const [contacts, setContacts] = useState([{ ContactName: "", ContactNumber: "" }]);
-  const [, forceUpdate] = useState();
+  
   let serverIP = window.location.hostname
   const fetchURL = "http://"+serverIP+":8081/fetchAllContacts";
   const addURL = "http://"+serverIP+":8081/addContact";
@@ -26,38 +63,53 @@ function App() {
     var contactName = evnt.target.elements.contactName.value
     var contactNumber = evnt.target.elements.contactNumber.value
     console.log("Add Contact "+ contactName +","+ contactNumber);
-    const addContactURL = addURL + "?contactName=" +contactName+"&contactNumber="+contactNumber;
+    const addContactURL = addURL + "?contactName=" +contactName
+            +"&contactNumber="+contactNumber;
     console.log(addContactURL)
     fetch(addContactURL)
       .then((response) => {
         console.log(response.json)
+        window.location.reload(false);
       });
     return;
   };
   
   return (
-    <div className="App">
-      <h1>Contact List App</h1>
+    <div className="App" >
       <br/>
-      <h2>Contact List</h2>
-      <table>
-        <tr><td>Contact Name</td><td>Contact Number</td></tr>
-          {contacts.map((value, key) => {
-            return <tr><td>{value.ContactName}</td><td>{value.ContactNumber}</td></tr>
-          })}
-      </table>
+      <h1>Contact List</h1>
       <br/>
+      <TableContainer component={Paper}>
+        <Table  sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead><StyledTableCell>Contact Name</StyledTableCell><StyledTableCell>Contact Number</StyledTableCell></TableHead>
+          <TableBody>
+            {contacts.map((value, key) => {
+              return <TableRow><StyledTableCell>{value.ContactName}</StyledTableCell><StyledTableCell>{value.ContactNumber}</StyledTableCell></TableRow>
+            })}
+          </TableBody>
+        </Table>
+     </TableContainer>
       <h2>Add New Contact </h2>
       <div id="addContactForm">
-        <form onSubmit={addContact}>
-            <label for="fname">Name :</label><br/>
-            <input type="text" id="contactName" name="contactName"/><br/>
-            <label for="lname">Contact Number:</label><br/>
-            <input type="text" id="contactNumber" name="contactNumber"/>
-            <button type="submit">Add Contact</button>
+       <form onSubmit={addContact}>
+       <TableContainer component={Paper}>
+        <Table  sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableBody>
+              <TableRow><StyledTableCell> <label for="fname">Name :</label></StyledTableCell>
+                <StyledTableCell><input type="text" id="contactName" name="contactName"/></StyledTableCell>
+              </TableRow>
+              <TableRow><StyledTableCell> <label for="lname">Contact Number:</label></StyledTableCell>
+                <StyledTableCell><input type="text" id="contactNumber" name="contactNumber"/></StyledTableCell>
+              </TableRow>
+              <TableRow><StyledTableCell> <button type="submit">Add Contact</button></StyledTableCell></TableRow>
+          </TableBody>
+        </Table>
+     </TableContainer>
+
+            
         </form>
       </div>
-
+      <br/>
     </div>
   );
 }
